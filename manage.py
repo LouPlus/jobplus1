b6f24c9a2bdaf3d1d32121cd31a604725b06645c
Edwin Yang <edwinymc@icloud.com>
2017-12-15 12:54:44 +0800

    manage requirement create

--- manage.py
+++ manage.py
@@ -1,7 +1,14 @@
 from jobplus.app import create_app
 
+<<<<<<< HEAD
 #使用开发环境配置
 app = create_app('development')
+=======
+
+app = create_app('development')
+app.config['SECRET_KEY'] = 'very very secret key'
+
+>>>>>>> manage requirement create
 
 if __name__ == '__main__':
     app.run()
