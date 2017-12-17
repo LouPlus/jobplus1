4b04156e2b430eb19a76b742088b6b0d9892976e
wmn7 <18811371255@163.com>
2017-12-17 20:48:44 +0800

    edit database and add database test

--- jobplus/app.py
+++ jobplus/app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+from jobplus.models import db
 from jobplus.config import configs
 
 #注册蓝图的函数
@@ -11,7 +12,8 @@ def register_blueprints(app):
 
 #用于将flask拓展注册到app
 def register_extensions(app):
-    pass
+    db.init_app(app)
+    Migrate(app,db)
 
 def create_app(config):
     """可以根据传入的config名称,加载不同的配置
--- jobplus/models/models.py
+++ jobplus/models/models.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from flask_migrate import Migrate
+
+from .base_models import db,Base
 
 """临时测试用"""
 """
@@ -17,15 +18,7 @@ db = SQLAlchemy(app)
 """临时测试用"""
 
 
-db = SQLALchemy()
-
-class Base(db.Model):
-    """时间戳基类"""
-    __abstract__ = True
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-
-class User(Base):
+class User(Base,UserMixin):
     __tablename__ = 'user'
 
     ROLE_EMPLOYEE = 10
@@ -33,15 +26,17 @@ class User(Base):
     ROLE_ADMIN = 30
 
     id = db.Column(db.Integer, primary_key=True)
-    role = db.Column(db.SmallInteger, default=ROLE_EMPLOYEE)
     name = db.Column(db.String(32), unique=True, index=True, nullable=False)
     email = db.Column(db.String(64), unique=True, index=True, nullable=False)
     _password = db.Column('password', db.String(256), nullable=False)
+    #这里存logo_img的url
     logo_img = db.Column(db.String(128))
-    #company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
-    company_id = db.relationship('Company', uselist=False)
-    #employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'))
+    #连接到公司的关系1-1关系
+    company = db.relationship('Company', uselist=False)
+    #连接到求职者关系1-1关系
     employee = db.relationship('Employee', uselist=False)
+    #用户权限
+    role = db.Column(db.SmallInteger, default=ROLE_EMPLOYEE)
 
     def __repr__(self):
         return '<User:{}>'.format(self.name)
@@ -55,6 +50,8 @@ class User(Base):
         self._password = generate_password_hash(orig_password)
 
     def check_password_hash(self, password):
+        """判断用户输入的密码和存储的hash密码是否相等
+        """
         return check_password_hash(self._password, password)
 
     @property
@@ -69,59 +66,73 @@ class User(Base):
     def is_emploee(self):
         return self.role == self.ROLE_EMPLOYEE
 
+
 import enum
 class SexType(enum.Enum):
     NONE = 0
     MALE = 1
     FEMALE = 2
 
+#求职者表
 class Employee(Base):
     __tablename__ = 'employee'
 
     id = db.Column(db.Integer, primary_key=True)
+
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship('User', uselist=False)
+
     sex = db.Column(db.Enum(SexType), default=SexType.NONE)
     location = db.Column(db.String(64))
     description = db.Column(db.String(256))
     resume = db.Column(db.String(128))
 
+#企业表
 class Company(Base):
     __tablename__ = 'company'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),index=True)
     user = db.relationship('User', uselist=False)
+
     website = db.Column(db.String(128))
     oneword = db.Column(db.String(64))
     description = db.Column(db.String(256))
 
+#职位表
 class Job(Base):
     __tablename__ = 'job'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
+    name = db.Column(db.String(64)) #不同公司可以有一样名字的职位
     wage = db.Column(db.String(64))
     location = db.Column(db.String(64))
+
     company_id = db.Column(db.Integer, db.ForeignKey('company.id', ondelete = 'CASCADE'))
-    company = db.relationship('Company')
-    description = db.Column(db.String(64))
-    requirement = db.Column(db.String(64))
+    company = db.relationship('Company',uselist=False)
+
+    description = db.Column(db.String(256))
+    requirement = db.Column(db.String(256))
 
 class Qualify_Type(enum.Enum):
-    UNREAD = 0
-    READ = 1
-    REFUSE = 2
-    UNREFUSE = 3
+    UNREAD = 0 #未被阅读
+    READ = 1 #已被阅读
+    REFUSE = 2 #拒绝该简历
+    ACCEPT = 3 #接受该简历
 
+#投递表
 class Send(Base):
     __tablename__ = 'send'
 
     id = db.Column(db.Integer, primary_key=True)
+
     company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
     job_id = db.Column(db.Integer, db.ForeignKey('job.id'))
     employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'))
+
     resume = db.relationship('Employee')
+
     qualify = db.Column(db.Enum(Qualify_Type), default = Qualify_Type.UNREAD)
 
 """临时测试用"""
