42ba84dd759252e535fb7c67d108874b83e8254d
luo <530122286@qq.com>
2017-12-18 18:42:47 +0800

    models放到主目录修改blueprint

--- jobplus/app.py
+++ jobplus/app.py
@@ -2,16 +2,21 @@ from flask import Flask,render_template
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+from jobplus.models import db
 from jobplus.config import configs
 
 #注册蓝图的函数
 def register_blueprints(app):
-    pass
+    from .handlers import front
+    app.register_blueprint(front)
 
 
 #用于将flask拓展注册到app
 def register_extensions(app):
-    pass
+    db.init_app(app)
+    Migrate(app,db)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
 
 def create_app(config):
     """可以根据传入的config名称,加载不同的配置
--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,6 +1,10 @@
 from flask import Blueprint,render_template,flash,redirect,url_for
 from flask import current_app,request
 from flask_login import login_user,logout_user,login_required
-
+from jobplus.models import User
 
 front = Blueprint('front',__name__)
+
+@front.route('/')
+def index():
+    return render_template('base.html')
