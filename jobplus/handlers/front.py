25d8a256a64e827eb6cea35191dfa5ecc059cb7e
Edwin Yang <edwinymc@icloud.com>
2017-12-21 13:11:08 +0800

    update

--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,27 +1,15 @@
 from flask import Blueprint,render_template,flash,redirect,url_for
 from flask import current_app,request
 from flask_login import login_user,logout_user,login_required
-from jobplus.models import User
-from jobplus.forms import UserProfile
+
 
 front = Blueprint('front',__name__)
 
 @front.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
 
 
 
 
-###Profile Config by EdwinYang2000
-@front.route('/user/profile/<int:user_id>',methods=['GET','POST'])
-@login_required     #role/employee decorators to be fixed later
-def profile(user_id):
-    user = User.query.get_or_404(user_id)
-    form = UserProfile(obj=user)
-    if form.validate_on_submit():
-        form.update_user(user)
-        flash('用户简历更新成功','success')
-        return redirect(url_for('front.profile'))
-    return render_template('profile.html',form=form,user=user)
 
--- jobplus/handlers/user.py
+++ jobplus/handlers/user.py
@@ -1,6 +1,21 @@
 from flask import Blueprint,render_template,flash,redirect,url_for
 from flask import current_app,request
 from flask_login import login_user,logout_user,login_required
-
+from jobplus.models import User
+from jobplus.forms import UserProfile
 
 user = Blueprint('user',__name__)
+
+
+
+###Profile Config by EdwinYang2000
+@user.route('/user/profile/<int:user_id>',methods=['GET','POST'])
+@login_required     #role/employee decorators to be fixed later
+def profile(user_id):
+    user = User.query.get_or_404(user_id)
+    form = UserProfile(obj=user)
+    if form.validate_on_submit():
+        form.update_user(user)
+        flash('用户简历更新成功','success')
+        return redirect(url_for('user.profile'))
+    return render_template('profile.html',form=form,user=user)
