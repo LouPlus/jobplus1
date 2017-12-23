7aa7c5acbe2b21f0ea2f601037ea41544b87aeda
wmn7 <18811371255@163.com>
2017-12-23 19:54:39 +0800

    edit userprofile

--- jobplus/forms/user_register_forms.py
+++ jobplus/forms/user_register_forms.py
@@ -2,13 +2,20 @@ from .base import *
 from jobplus.models import db, User
 
 
-#注册表 # by EdwinYang2000
-
-class RegisterForm(FlaskForm):
-    name = StringField('用户名', validators=[Required(), Length(3,24)])
+#Profile表 # by EdwinYang2000
+# edit wmn7
+class UserRegister(FlaskForm):
+    name = StringField('求职者姓名', validators=[Required(), Length(4,24)])
     email = StringField('邮箱', validators=[Required(), Email(message='请输入合法email地址')])
-    password = PasswordField('密码', validators=[Required(), Length(6,24)])
-    repeat_password = PasswordField('重复密码',validators=[Required(), EqualTo('password')])
+    password = PasswordField('密码',validators=[Required()])
+    repeat_password = PasswordField('重复密码',validators=[Required(),EqualTo('password')])
+    image = StringField('头像链接')
+
+    sex = StringField('性别',validators=[Required()])
+    location = StringField('城市',validators=[Required()])
+    description = StringField('自我介绍', validators=[Required(),Length(10,256)])
+    resume = StringField('简历地址')
+
     submit = SubmitField('提交')
 
     def create_user(self):
@@ -28,7 +35,8 @@ class RegisterForm(FlaskForm):
         if User.query.filter_by(email=field.data).first():
             raise ValidationError('邮箱已经存在')
 
-    def update_user(self, user):
+
+    def create_user(self, user):
         self.populate_obj(user)
         db.session.add(user)
         db.session.commit()
