75e4ca3b5abaa49d714037623c27bfb52980bf8f
Edwin Yang <edwinymc@icloud.com>
2017-12-23 15:48:17 +0800

    profile ok

--- jobplus/forms/user_profile.py
+++ jobplus/forms/user_profile.py
@@ -1,5 +1,5 @@
 from .base import *
-from jobplus.models import db, User
+from jobplus.models import db, User, Employee
 
 
 #Profile表 # by EdwinYang2000
@@ -7,25 +7,27 @@ class UserProfile(FlaskForm):
     name = StringField('求职者姓名', validators=[Required(), Length(4,24)])
     email = StringField('邮箱', validators=[Required(), Email(message='请输入合法email地址')])
     password = PasswordField('密码')
-    sex = StringField('性别')
+    submit = SubmitField('提交')
+    #sex = StringField('性别')
     location = StringField('城市')
-    description = StringField('自我介绍', validators=[Required(),Length(10,256)])
+    description = StringField('自我介绍', validators=[Required(), Length(10, 256)])
     resume = StringField('简历地址')
     submit = SubmitField('提交')
 
 
-    def validate_name(self,field):
-        if User.query.filter_by(name=field.data).first():
-            raise ValidationError('求职者姓名已经存在')
+    def updated_profile(self, user):
+        user.name = self.name.data
+        user.email = self.email.data
+        if self.password.data:
+            user.password = self.password.data
 
+        if user.user_detail:
+            user_detail = user.user_detail
 
-    def validate_email(self,field):
-        if User.query.filter_by(email=field.data).first():
-            raise ValidationError('邮箱已经存在')
-
-
-    def update_user(self, user):
-        self.populate_obj(user)
+        else:
+            user_detail = Employee()
+            user_detail.user_id = user.id
+        self.populate_obj(user_detail)
         db.session.add(user)
+        db.session.add(user_detail)
         db.session.commit()
-        return user
--- jobplus/handlers/user.py
+++ jobplus/handlers/user.py
@@ -1,6 +1,6 @@
 from flask import Blueprint,render_template,flash,redirect,url_for
 from flask import current_app,request
-from flask_login import login_user,logout_user,login_required
+from flask_login import login_user,logout_user,login_required, current_user
 from jobplus.models import User
 from jobplus.forms import UserProfile
 
@@ -9,13 +9,15 @@ user = Blueprint('user',__name__)
 
 
 ###Profile Config by EdwinYang2000
-@user.route('/user/profile/<int:user_id>',methods=['GET','POST'])
+@user.route('/user/profile', methods=['GET','POST'])
 @login_required     #role/employee decorators to be fixed later
-def profile(user_id):
-    user = User.query.get_or_404(user_id)
+def profile():
+    user = current_user.user_detail
     form = UserProfile(obj=user)
+    form.name.data = current_user.name
+    form.email.data = current_user.email
     if form.validate_on_submit():
-        form.update_user(user)
+        form.updated_profile(current_user)
         flash('用户简历更新成功','success')
         return redirect(url_for('user.profile'))
     return render_template('profile.html',form=form,user=user)
